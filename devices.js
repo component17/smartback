1cfacb56edf49765d0538a106d479913e4fa6524
evgenijspilka <mltec.495@gmail.com>
2018-10-02 22:54:57 +0300

    Update!!!

--- devices.js
+++ devices.js
@@ -3,28 +3,28 @@ module.exports = [
         namespace: 'lineOne',
         name: 'Линия 1',
         gpio: 16,
-        default: false,
+        default: true,
         indicator: 0
     },
     {
         namespace: 'lineTwo',
         name: 'Линия 2',
         gpio: 20,
-        default: false,
+        default: true,
         indicator: 1
     },
     {
         namespace: 'kitchen',
         name: 'Кухня',
         gpio: 21,
-        default: false,
+        default: true,
         indicator: 2
     },
     {
         namespace: 'boss',
         name: 'Этаж 2',
         gpio: 12,
-        default: false,
+        default: true,
         indicator: 3
     },
 ];
